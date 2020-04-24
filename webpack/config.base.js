3b1bf1f99dc9ce445a7729203bf7d610f68bb34d
Cora Underhill <coraunderhill@gmail.com>
2020-04-23 17:13:53 -0700

    Added external Google API library

--- webpack/config.base.js
+++ webpack/config.base.js
@@ -3,32 +3,35 @@ const DotEnvPlugin = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // Import core configuration values
-const core  = require('./config.core.js');
+const core = require('./config.core.js');
 
 // Base configuration
 module.exports = {
-	context: core.context,
-	entry: './src',
-	module: {
-		rules: [
-			{ // JavaScript and JSX
-				test: /\.jsx?/,
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-				options: {
-					plugins: ['@babel/plugin-transform-runtime'],
-					presets: ['@babel/preset-env', '@babel/preset-react'],
-				},
-			},
-		],
-	},
-	output: core.output,
-	plugins: [
-		new DotEnvPlugin(),
-		new HtmlWebpackPlugin({
-			cache: false,
-			template: 'src/static/html/template.html',
-		}),
-	],
-	resolve: core.resolve,
+  context: core.context,
+  entry: './src',
+  externals: {
+    gapi: 'gapi',
+  },
+  module: {
+    rules: [
+      { // JavaScript and JSX
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          plugins: ['@babel/plugin-transform-runtime'],
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
+  },
+  output: core.output,
+  plugins: [
+    new DotEnvPlugin(),
+    new HtmlWebpackPlugin({
+      cache: false,
+      template: 'src/static/html/template.html',
+    }),
+  ],
+  resolve: core.resolve,
 };

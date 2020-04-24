2021ad46c90f62b6886457788b0428df2b779afe
Cora Underhill <coraunderhill@gmail.com>
2020-04-23 17:13:39 -0700

    Removed resolve alias from Webpack core config

--- webpack/config.core.js
+++ webpack/config.core.js
@@ -3,16 +3,11 @@ const path = require('path');
 
 // Set core configuration values to be referenced
 module.exports = {
-	context: path.resolve(__dirname, '../app'),
-	output: {
-			path: path.resolve(__dirname, '../app/build'),
-	},
-	resolve: {
-		alias: {
-			Common: path.resolve(__dirname, '../app/src/common'),
-			Components: path.resolve(__dirname, '../app/src/components'),
-			Html: path.resolve(__dirname, '../app/src/static/html'),
-		},
-		extensions: [ '.js', '.jsx', '.json' ],
-	},
+  context: path.resolve(__dirname, '../app'),
+  output: {
+    path: path.resolve(__dirname, '../app/build'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
 };

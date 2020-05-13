309f16d65a8179184c4dad3aa0636300714b8142
Cora <cora@Coras-MacBook-Pro.local>
2020-05-12 23:07:49 -0700

    Set app start to init API promise then update view

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,11 @@
 // Import Node modules
 import { platform } from 'os';
 
+// Import React renderer
+import { updateView } from './common/renderer';
+
 // Import YouTube Data API client
-import { list } from './common/client';
+import list from './common/client';
 
 // Import CSS frameworks
 import photon from './static/css/_photon';
@@ -15,11 +18,13 @@ switch (platform) {
     break;
 }
 
-// Set API request parameters
+// Set up API request and execute
 const params = {
   chart: 'mostPopular',
   part: 'id,snippet',
   regionCode: 'US',
 };
-
-list(params);
+list(params).then(res => {
+  console.log(res.data);
+  updateView(res.data);
+});

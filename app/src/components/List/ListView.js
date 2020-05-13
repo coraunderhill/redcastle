04de89789e1ec940322d6afa030faf7d027380ff
Cora <cora@Coras-MacBook-Pro.local>
2020-05-12 20:43:47 -0700

    Added video-list Sass file and additional snippet details

--- app/src/components/List/ListView.js
+++ app/src/components/List/ListView.js
@@ -1,6 +1,9 @@
 // Import Node modules
 import React from 'react';
 
+// Import resources
+import './video-list';
+
 /**
  * Video list view
  * @param {Object} props React properties
@@ -15,12 +18,15 @@ const ListView = props => {
   // Iterate over items in object and map to new list
   let listItems = [];
   for (let [key, val] of Object.entries(items)) {
-    const desc = val.snippet.description;
+    const date = new Date(val.snippet.publishedAt).toDateString();
     const id = val.id;
+    const time = new Date(val.snippet.publishedAt).toLocaleTimeString();
     const title = val.snippet.title;
+
     listItems.push(
       <div className="video-list-item" key={id}>
         <span className="video-list-item-title">{title}</span>
+        <span className="video-list-item-date">Published on {date} at {time}</span>
       </div>
     );
   }

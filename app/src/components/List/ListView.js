407dd433373fe7a34bc86eb0d437ffffdb0fb869
Cora <cora@Coras-MacBook-Pro.local>
2020-05-12 20:51:39 -0700

    Added thumbnails to ListView

--- app/src/components/List/ListView.js
+++ app/src/components/List/ListView.js
@@ -18,15 +18,18 @@ const ListView = props => {
   // Iterate over items in object and map to new list
   let listItems = [];
   for (let [key, val] of Object.entries(items)) {
+    const channel = val.snippet.channelTitle;
     const date = new Date(val.snippet.publishedAt).toDateString();
     const id = val.id;
     const time = new Date(val.snippet.publishedAt).toLocaleTimeString();
+    const thumb = val.snippet.thumbnails.default.url;
     const title = val.snippet.title;
 
     listItems.push(
       <div className="video-list-item" key={id}>
+        <span className="video-list-item-thumb"><img src={thumb} /></span>
         <span className="video-list-item-title">{title}</span>
-        <span className="video-list-item-date">Published on {date} at {time}</span>
+        <span className="video-list-item-date">{channel} &ndash; {date} at {time}</span>
       </div>
     );
   }

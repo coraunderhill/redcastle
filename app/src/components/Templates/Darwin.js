3794967b22e6094d3655502aaa3104ce3d74528d
Cora <cora@Coras-MacBook-Pro.local>
2020-05-13 15:53:19 -0700

    Added macOS template

---
+++ app/src/components/Templates/Darwin.js
@@ -0,0 +1,29 @@
+// Import Node modules
+import React from 'react';
+
+// Import React components
+import Header from '../Header/Header';
+import Nav from '../Nav/Nav';
+
+/**
+ * macOS app template
+ * @param {Object} view React component to be served
+ * @returns {Object} React component
+ */
+const Darwin = view => (
+  <div className="window">
+    <Header />
+    <div className="window-content">
+      <div className="pane-group">
+        <div className="pane-sm sidebar">
+          <Nav />
+        </div>
+        <div className="pane">
+          {view}
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Darwin;

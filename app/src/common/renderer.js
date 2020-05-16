49967751162c018f38fbffb0893a62102ead094d
Cora Underhill <coraunderhill@gmail.com>
2020-05-16 01:42:24 -0700

    Moved theme functionality to new module

--- app/src/common/renderer.js
+++ app/src/common/renderer.js
@@ -1,30 +1,18 @@
 // Import Node modules
-import { platform } from 'os';
 import React from 'react';
 import { render } from 'react-dom';
 
-// Import CSS frameworks
-import photon from '../static/css/_photon';
-
-// Import React components
-import Darwin from '../components/Templates/Darwin';
+// Import template handler
+import { getTheme } from './theme';
 
 /**
  * Update React view
  * @param {Object} view React component to load with template
  */
-const updateView = (view) => {
-  let App;
-  let cssFramework;
+const updateView = view => {
 
-  // Set resources depending on host OS
-  if (platform == 'darwin') {
-    App = Darwin(view);
-    cssFramework = photon;
-  }
-
-  // Render app
-  render(App, document.getElementById('root'));
+  const Theme = getTheme();
+  render(Theme(view), document.getElementById('root'));
 }
 
 export default updateView;

d39fcf051824abf65013178954fb932f57399021
Cora <cora@Coras-MacBook-Pro.local>
2020-05-17 20:35:54 -0700

    Added WatchView component

---
+++ app/src/components/Watch/WatchView.js
@@ -0,0 +1,48 @@
+// Import Node modules
+import React from 'react';
+
+// Import React components
+import Player from '../Player/Player';
+
+/**
+ * Video page view
+ * @param {Object} props Component properties
+ * @param {Array} props.videoDetails Video details to be rendered
+ * @returns {Object} React component
+ */
+const WatchView = props => {
+
+  const { videoDetails } = props;
+
+  // Deconstruct video details
+  const {
+    id,
+    snippet,
+    stats,
+  } = videoDetails;
+
+  // Rinse and repeat with the detail objects
+  const {
+    channelTitle,
+    description,
+    publishedAt,
+    title,
+  } = snippet;
+
+  return (
+    <div className="watch-video">
+      <div className="watch-player"><Player videoID={id} /></div>
+      <div className="watch-video-details">
+        <span className="watch-video-details-title">{title}</span>
+        <br />
+        <span className="watch-video-details-channel-title">{channelTitle}</span>
+        &nbsp;–&nbsp;
+        <span className="watch-video-details-publishedAt">{publishedAt}</span>
+        <span className="watch-video-details-description">{description}</span>
+      </div>
+    </div>
+  );
+
+}
+
+export default WatchView;

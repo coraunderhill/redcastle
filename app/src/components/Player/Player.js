c3aa71866514b41215cc7ad89e4d4e3754c3f290
Cora <cora@Coras-MacBook-Pro.local>
2020-05-17 20:37:10 -0700

    Added Player component template

---
+++ app/src/components/Player/Player.js
@@ -0,0 +1,20 @@
+// Import Node modules
+import React from 'react';
+
+/**
+ *  Video player component
+ * @param {Object} props Component properties
+ */
+const Player = props => {
+
+  const { videoID } = props;
+
+  return (
+    <div className="video-player">
+      videoId
+    </div>
+  );
+
+}
+
+export default Player;

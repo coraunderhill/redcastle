0db6268c90c278aff0d6c74c2fdf6281d2d3cf3c
Cora <cora@Coras-MacBook-Pro.local>
2020-05-17 20:40:49 -0700

    Added missing documentation

--- app/src/components/Nav/NavView.js
+++ app/src/components/Nav/NavView.js
@@ -1,17 +1,18 @@
-// Import modules
+// Import Node modules
 import React from 'react';
 
 /**
  * Navigation view
  * @param {Object} props Component properties
+ * @param {Object} props.items Navigation items to be rendered
  * @returns {Object} React component
  */
 const NavView = props => {
 
   const { items } = props;
 
+  // Iterate over items and map to new list
   const navItems = [];
-
   for (let [key, val] of Object.entries(items)) {
     const {
       icon,

b6bc10247a693cce61c65869d98375be1389ac18
Cora <cora@Coras-MacBook-Pro.local>
2020-05-17 20:41:09 -0700

    Set Electron to start server on ready

--- app/src/electron.js
+++ app/src/electron.js
@@ -1,11 +1,17 @@
 // Import Node modules
 import { BrowserWindow, app } from 'electron';
+import { startServer } from './common/server';
 
 // Instantiate the window to prevent eaten into memory
 let window;
 
 // Create browser window on app ready
 app.on('ready', () => {
+
+  // Start local webserver to host app content
+  startServer();
+
+  // Create a new app window and load local server
   window = new BrowserWindow({
     height: 600,
     titleBarStyle: 'hidden',
@@ -13,6 +19,6 @@ app.on('ready', () => {
       nodeIntegration: true,
     },
     width: 800,
-  });
-  window.loadFile('./index.html');
+  }).loadURL('http://127.0.0.1:18451/');
+
 });

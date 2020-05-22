f343ecf2cdd6a33b11ebfc7456e5dc4c8a61c98d
Cora <cora@Coras-MacBook-Pro.local>
2020-05-22 16:16:12 -0700

    Set navigate to be in charge of API calls again

--- app/src/common/navigate.js
+++ app/src/common/navigate.js
@@ -2,13 +2,29 @@
 import React from 'react';
 
 // Import React components
-import Watch from 'Components/Watch/Watch';
+import Watch from '../components/Watch/Watch';
 
 // Import React renderer
 import updateView from './renderer';
+
+// Import API client
+import { list } from './client';
 
 /**
  * Render the Watch component with a selected video
  * @param {string} videoID ID of the video to display
  */
-export const watchVideo = videoID => updateView(<Watch videoID={videoID} />);
+export const watchVideo = videoID => {
+
+  // Construct API parameters
+  const params = {
+    id: videoID,
+    part: 'snippet,statistics',
+  };
+
+  // Query API and update view
+  list(params).then(res => (
+    updateView(<Watch data={res.data} />)
+  ));
+
+}

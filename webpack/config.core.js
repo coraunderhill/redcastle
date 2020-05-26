7a95ab593eab46a1f3a5ffec52a53f3c25a60e54
Cora <cora@Coras-MacBook-Pro.local>
2020-05-25 21:32:12 -0700

    Fixed Webpack config bundling JSON files wrong

--- webpack/config.core.js
+++ webpack/config.core.js
@@ -8,7 +8,7 @@ module.exports = {
   module: {
     rules: {
       js: { // JavaScript
-        test: /\.js/,
+        test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
         options: {

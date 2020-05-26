7304c2b6fdbb70c56b0a2df33a964d3df64cc7a4
Cora <cora@Coras-MacBook-Pro.local>
2020-05-25 21:39:42 -0700

    Added mostPopular method to navigate module

--- app/src/common/navigate.js
+++ app/src/common/navigate.js
@@ -2,13 +2,27 @@
 import React from 'react';
 
 // Import React components
-import Watch from '../components/Watch/Watch';
+import List from 'Components/List/List';
+import Watch from 'Components/Watch/Watch';
 
 // Import React renderer
 import updateView from './renderer';
 
 // Import API client
 import { list } from './client';
+
+export const mostPopular = () => {
+
+  const params = {
+    chart: 'mostPopular',
+    part: 'id,snippet',
+    regionCode: 'US',
+  };
+
+  list(params).then(res => (
+    updateView(<List data={res.data} />)
+  ));
+}
 
 /**
  * Render the Watch component with a selected video

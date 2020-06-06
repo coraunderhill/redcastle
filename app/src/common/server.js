56061c059d43990d24a8772effb72765ef773011
Cora <cora@Coras-MacBook-Pro.local>
2020-06-05 23:53:02 -0700

    Changed server module to  handle requests inside

--- app/src/common/server.js
+++ app/src/common/server.js
@@ -1,13 +1,29 @@
 // Import Node modules
+import { resolve } from 'path';
 import sl from 'server-lite';
+import { parse } from 'url';
 
 /**
  * Starts a local webserver for hosting files
  */
 export const startServer = () => {
 
-  // Create the handler
-  const handler = new sl.handler(undefined, './app/build', '/index.html');
+  /**
+   * Server utility functions
+   */
+  const utils = new sl.utils();
+
+  /**
+   * Full path to the build directory
+   * @type {string}
+   * @default
+   */
+  const build = resolve('./app/build');
+
+  /**
+   * Server request handler
+   */
+  const handler = new sl.handler(utils, build, '/index.html');
 
   /**
    * Handles HTTP requests
@@ -15,17 +31,48 @@ export const startServer = () => {
    * @param {Object} res Server response object
    */
   const onRequest = (req, res) => {
-    handler.simpleFileBasedWebServer(req, res);
+    /**
+     * Path from request body
+     * @type {string}
+     * @default
+     */
+    const path = parse(req.url).pathname;
+
+    /**
+     * If the request includes a period, get the requested extension
+     * @type {string|null}
+     * @default
+     */
+    const ext = (path.includes('.'))
+      ? path.split('.').slice(-1).pop()
+      : null;
+
+    /**
+     * Full path to file to respond with
+     * @type {string}
+     * @default
+     */
+    const file = (ext === null)
+      ? `${build}/index.html`
+      : `${build}${path}`;
+
+    utils.respondWithFileFromPath(file, res);
   }
 
-  // Set server configuration
+  /**
+   * Sets the server configuration
+   */
   const config = new sl.config({
     onRequest: onRequest,
     port: 18451,
   });
 
-  // Start server with config object
+  /**
+   * Creates the server object
+   */
   const server = new sl.server.http(config);
+
+  // Start the server
   server.start();
 
 };

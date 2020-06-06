fa45087c8777b37119d758e8f50745c5c9155e26
Cora <cora@Coras-MacBook-Pro.local>
2020-06-05 23:51:44 -0700

    Added missing documentation to navItems

--- app/src/common/navItems.js
+++ app/src/common/navItems.js
@@ -1,11 +1,24 @@
 // Import menu from JSON file
 import menuItems from 'Static/json/menu-nav.json';
 
+/**
+ * Parses `menu-nav.json`
+ * @returns {Array} Array of navigation item objects
+ */
 const navItems = () => {
 
-  // Iterate over menu items and map to new list
+  /**
+   * Navigation items
+   * @type {Array}
+   */
   const items = [];
+
+  // Iterate over menu items and map to new list
   for (let [key, val] of Object.entries(menuItems)) {
+    /**
+     * Navigation item from menu object
+     * @type {Object}
+     */
     const item = {
       icon: val.icon,
       label: val.label,

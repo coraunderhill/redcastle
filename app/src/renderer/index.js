3ce86c4ee879eafa5400651fb7badb9177af26aa
Cora Underhill <coraunderhill@gmail.com>
2020-08-01 20:16:32 -0700

    Renderer index uses new navigate module code, reducing it from a lot

--- app/src/renderer/index.js
+++ app/src/renderer/index.js
@@ -1,37 +1,5 @@
-// Import Node modules
-import React from 'react';
+// Import navigation module
+import { home } from '@common/navigate';
 
-// Import React components
-import List from '@components/Global/List/List';
-
-// Import theme init
-import { initTheme } from '@components';
-
-// Import React renderer
-import updateView from '@common/renderer';
-
-// Import YouTube Data API client
-import { list } from '@common/client';
-
-/**
- * Initiates app
- */
-const start = () => {
-
-  // Set up API request and execute
-  const params = {
-    chart: 'mostPopular',
-    part: 'id,snippet',
-    regionCode: 'US',
-  };
-
-  // Call API then update view
-  list(params).then(res => {
-    // Pass the view to the renderer
-    updateView(<List data={res.data} />, '/');
-  });
-
-}
-
-initTheme();
-start();
+// Start the app
+home();

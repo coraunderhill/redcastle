e0ffcb4ac6e11bbc8453aa9ae54396cecf6e25f5
Cora Underhill <coraunderhill@gmail.com>
2020-08-01 20:47:15 -0700

    That was a messy merge and rebase, but now it's fixed?

---
+++ app/src/renderer/components/Darwin/Watch.js
@@ -0,0 +1,54 @@
+// Import modules
+import React from 'react';
+
+/**
+ * macOS video player component
+ * @param {Object} props Component properties
+ * @param {Object} props.data API response data
+ * @returns {JSX.Element|boolean} React component or false on error
+ */
+const Watch = props => {
+
+  const { data } = props;
+
+  // Make sure we have the correct API response
+  if (!data.kind || data.kind !== 'youtube#video') {
+    console.log('video expected from API in Watch component');
+    return false;
+  }
+
+  // Deconstruct video data
+  const { snippet, statistics } = data;
+
+  //
+
+  return null;
+
+};
+
+/**
+ * macOS video details component
+ * @param {Object} props Component properties
+ * @param {string} props.channel Channel name
+ * @param {string} props.text Video detail text
+ * @param {string} props.title Video title
+ * @returns {JSX.Element} React component
+ */
+export const WatchDetails = props => {
+
+    const {
+      channel,
+      text,
+      title
+    } = props;
+
+    return (
+      <div>
+        <h3>{title}</h3>
+        <span>{channel}</span>
+        <p>{text}</p>
+      </div>
+    )
+}
+
+export default Watch;

0fdd94c347af496e35211d41bd32185ff3c62e0e
Cora Underhill <coraunderhill@gmail.com>
2020-09-24 18:45:22 -0700

    Removed navItems module; added parseQueryString to helpers mod

--- app/src/renderer/common/helpers.js
+++ app/src/renderer/common/helpers.js
@@ -1,22 +1,61 @@
 /**
- * Parses Date object
- * @param {Date} date Date object
- * @returns {string} Localized date string
+ * Parses Date to localized date
+ * @param {string} date Date string
+ * @returns {string} Localized date
  */
 export const parseDate = date => {
-  const d = new Date();
+
+  /**
+   * @type {Date}
+   */
+  const d = new Date(date)
+
   return d.toLocaleDateString();
+
 }
 
 /**
- * Parses Date object
- * @param {Date} date Date object
- * @returns {string} Localized time string
+ * Parses a query string into an a new object
+ * @param {string} queryString Query string without path
+ * @returns {Object} Resulting object
+ */
+export const parseQueryString = queryString => {
+
+  /**
+   * @type {string[]}
+   */
+  const parts = queryString.split('&');
+
+  /**
+   * @type {Object}
+   */
+  const queries = {};
+
+  // Populate queries object
+  parts.forEach(val => {
+    const parts = val.split('=');
+    queries[parts[0]] = parts[1];
+  });
+
+  return queries;
+
+}
+
+/**
+ * Parses Date to localized time
+ * @param {string} date Date string
+ * @returns {string} Localized time
  */
 export const parseTime = date => {
-  const d = new Date();
+
+  /**
+   * @type {Date}
+   */
+  const d = new Date(date);
+
   return d.toLocaleTimeString([], {
     hour: 'numeric',
     minute: '2-digit',
   });
+
 };

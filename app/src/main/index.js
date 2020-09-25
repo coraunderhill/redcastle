0290656000af4d60d5e134cb4fb1b735fc331a04
Cora Underhill <coraunderhill@gmail.com>
2020-09-24 18:55:58 -0700

    I'm running out of things at this point
    I'm just tired

--- app/src/main/index.js
+++ app/src/main/index.js
@@ -1,26 +1,58 @@
-// Import modules
+/**
+ * @module main
+ */
+
 import { BrowserWindow, app } from 'electron';
-import { windowCfg } from './platform';
+import listen from './ipc';
 import { startServer } from './server';
 
-/**
- * Electron BrowserWindow.
- * Referenced early to prevent being eaten by garbage collection
- * @type {BrowserWindow}
- */
-let window;
+// Start local webserver to host app content
+startServer();
 
-// Create browser window on app ready
+// Start the IPC listeners
+listen();
+
+/**
+ * @type {Electron.BrowserWindowConstructorOptions}
+ */
+const config = {
+  height: 600,
+  minHeight: 480,
+  minWidth: 480,
+  scrollBounce: true,
+  titleBarStyle: 'hiddenInset',
+  vibrancy: 'sidebar',
+  visualEffectStates: 'followWindow',
+  webPreferences: {
+    nodeIntegration: true,
+  },
+  width: 800,
+};
+
+/**
+ * Main app window
+ * @type {null|Electron.BrowserWindow}
+ */
+export let window = null;
+
+/**
+ *  Dev tools window
+ * @type {null|Electron.BrowserWindow}
+ */
+let devTools = null;
+
+// Run when Electron preload has finished
 app.on('ready', () => {
 
-  // Start local webserver to host app content
-  startServer();
-
-  window = new BrowserWindow(windowCfg())
-  .loadURL('http://127.0.0.1:18451/');
+  window = new BrowserWindow(config)
+  devTools = new BrowserWindow();
+  window.loadURL('http://127.0.0.1:18451/');
+  window.webContents.setDevToolsWebContents(devTools.webContents);
+  window.webContents.openDevTools();
 
 });
 
+// Run when Electron generates a view
 app.on('web-contents-created', (e, contents) => {
 
   // Prevent _blank from creating new windows

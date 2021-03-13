1005bfdadf3b05580340d43b425bc3a36121a97c
Cora Underhill <coraunderhill@gmail.com>
2021-03-12 21:12:34 -0800

    Added isRecent to JS helpers

--- src/renderer/common/helpers.js
+++ src/renderer/common/helpers.js
@@ -1,4 +1,35 @@
 /**
+ * Checks if a given date is today
+ * @param {string} date Date string
+ * @returns {number} 1 for today, 0 for yesterday, -1 otherwise
+ */
+export const isRecent = date => {
+
+  const today = new Date();
+  const input = new Date(date);
+  const yesterday = new Date(today);
+
+  yesterday.setDate(yesterday.getDate() - 1);
+
+  const inDay = input.getDate();
+  const inMonth = input.getMonth();
+  const inYear = input.getFullYear();
+
+  if (
+    inDay === today.getDate()
+    && inMonth === today.getMonth()
+    && inYear === today.getFullYear()
+  ) { return 1 }
+  else if (
+    inDay === yesterday.getDate()
+    && inMonth === yesterday.getMonth()
+    && inYear === yesterday.getFullYear()
+  ) { return 0 }
+  else return -1;
+
+}
+
+/**
  * Parses Date to localized date
  * @param {string} date Date string
  * @returns {string} Localized date
